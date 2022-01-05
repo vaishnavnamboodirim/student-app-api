a8b98aa93a02d74db5085d643298011ef35c30a0
Vaishnav N Mangalam <44635792+vaishnavnamboodirim@users.noreply.github.com>
2022-01-05 16:34:12 +0530

    Add files via upload

--- backend/helper/helper.go
+++ backend/helper/helper.go
@@ -1,1 +1,66 @@
+package helper
 
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func ConnectDB() *mongo.Collection {
+	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
+	client, err := mongo.Connect(context.TODO(), clientOptions)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("connected to MongoDB")
+
+	collection := client.Database("student-app-api").Collection("students")
+
+	return collection
+}
+
+type ErrorResponse struct {
+	StatusCode   int    `json:"status"`
+	ErrorMessage string `json:"message"`
+}
+
+func GetError(err error, w http.ResponseWriter) {
+
+	log.Fatal(err.Error())
+	var response = ErrorResponse{
+		ErrorMessage: err.Error(),
+		StatusCode:   http.StatusInternalServerError,
+	}
+
+	message, _ := json.Marshal(response)
+
+	w.WriteHeader(response.StatusCode)
+	w.Write(message)
+}
+
+type Configuration struct {
+	Port             string
+	ConnectionString string
+}
+
+func GetConfiguration() Configuration {
+	err := godotenv.Load("./.env")
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	configuration := Configuration{
+		os.Getenv("PORT"),
+		os.Getenv("CONNECTION_STRING"),
+	}
+
+	return configuration
+}
