44109ffada051be2adfa01e8d64386963ba44d9d
Vaishnav N Mangalam <44635792+vaishnavnamboodirim@users.noreply.github.com>
2022-01-05 16:35:08 +0530

    Add files via upload

--- backend/models/models.go
+++ backend/models/models.go
@@ -1,1 +1,11 @@
+package models
 
+import "go.mongodb.org/mongo-driver/bson/primitive"
+
+type Student struct {
+	ID      primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
+	Name    string             `json:"name,omitempty" bson:"name,omitempty"`
+	MyClass string             `json:"myclass,omitempty" bson:"myclass,omitempty"`
+	Marks   string             `json:"marks,omitempty" bson:"marks,omitempty"`
+	RollNo  string             `json:"rollno,omitempty" bson:"rollno,omitempty"`
+}
